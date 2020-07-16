64eab2c3f30a88610988e97aaf7e55ba01afe68a
Avez-Anwar <avezanwar6@gmail.com>
2020-07-16 21:14:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./components/PostList";
+import "./App.css";
+import PostList from "./components/PostList";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      posts: [],
+      error: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        console.log(response);
+        this.setState({ posts: response.data });
+      })
+      .catch((error) => {
+        console.log(error);
+        this.setState({ errorMsg: "Error retreiving data" });
+      });
+  }
+
+  render() {
+    const { posts, errorMsg } = this.state;
+    return (
+      <div className="App">
+        List of Posts
+        {posts.length
+          ? posts.map((post) => <div key={post.id}>{post.title}</div>)
+          : null}
+        {errorMsg ? <div>{errorMsg}</div> : null}
+      </div>
+    );
+  }
 }
-
-export default App;
