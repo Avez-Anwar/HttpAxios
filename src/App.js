939a38fc7b07ce2292fc22c7e20ebc6d39f376c4
Avez-Anwar <avezanwar6@gmail.com>
2020-08-25 18:10:36 +0530

    react redux

--- src/App.js
+++ src/App.js
@@ -1,42 +1,16 @@
-import React, { Component } from "react";
-import "./components/PostList";
+import React from "react";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import CakeContainer from "./components/CakeContainer";
 import "./App.css";
-import PostList from "./components/PostList";
-import axios from "axios";
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      posts: [],
-      error: [],
-    };
-  }
-
-  componentDidMount() {
-    axios
-      .get("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => {
-        console.log(response);
-        this.setState({ posts: response.data });
-      })
-      .catch((error) => {
-        console.log(error);
-        this.setState({ errorMsg: "Error retreiving data" });
-      });
-  }
-
-  render() {
-    const { posts, errorMsg } = this.state;
-    return (
+function App() {
+  return (
+    <Provider store={store}>
       <div className="App">
-        List of Posts
-        {posts.length
-          ? posts.map((post) => <div key={post.id}>{post.title}</div>)
-          : null}
-        {errorMsg ? <div>{errorMsg}</div> : null}
+        <CakeContainer />
       </div>
-    );
-  }
+    </Provider>
+  );
 }
+export default App;
